904c790d175f65df86667d4f7a3c6d28a8d6f134
Nate <natestorms416@gmail.com>
2023-04-18 11:50:09 -0500

    finishing up event detection, prep for push to main

--- lightsaberMain/lightsaberMain.ino
+++ lightsaberMain/lightsaberMain.ino
@@ -11,22 +11,7 @@ void loop() {
   int delayTime = 100;
   // put your main code here, to run repeatedly:
   int event = eventDetection(20,30);
-  switch (event) {
-    case 1:
-      Serial.print("Swipe detected!\n");
-      break;
-    case 2:
-      Serial.print("Clash Detected!\n");
-      break;
-    case 3:
-      Serial.print("Stab detected!\n");
-      break;
-    case 4:
-      Serial.print("Stab clash detected!\n");
-      break;
-    default:
-      //defualts
-      break;  
+  
   }
   delay(delayTime);
 }
